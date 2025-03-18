6014ce2f3da1a217399bb4e19972f2d3a68a4129
subarusu <110146246+subarusu@users.noreply.github.com>
2025-03-19 04:22:22 +0700

    Create list.js

---
+++ list.js
@@ -0,0 +1,6 @@
+const daftarBarang = [
+    { id: "buku", nama: "Buku" },
+    { id: "pensil", nama: "Pensil" },
+    { id: "penghapusxxxxxxxxxx", nama: "Penghapus" },
+    { id: "pulpen", nama: "Pulpen" }
+];
